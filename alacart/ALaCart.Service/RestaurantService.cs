02e72ecd2ba401b1d58552de13e514786d8cd1c6
Terrence Adams <terrence.adams@sync1systems.com>
2019-05-15 17:34:21 -0500

    added interface

---
+++ alacart/ALaCart.Service/RestaurantService.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using System.Text;
+using ALaCart.Data;
+using ALaCart.Models;
+using ALaCart.Data.Interfaces;
+
+namespace ALaCart.Service
+{
+    public interface IRestaurantService
+    {
+        Restaurant GetById();
+
+        ICollection<Restaurant> GetAll();
+
+    }
+
+
+
+    public class RestaurantService : IRestaurantService
+    {
+        public ICollection<Restaurant> GetAll()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Restaurant GetById()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
